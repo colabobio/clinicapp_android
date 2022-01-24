99549d7424531f19b7e49087960c26321ed95692
codeanticode <andres.colubri@gmail.com>
2022-01-23 20:49:48 -0500

    isNetworkConnected always returns false to force offline operation

--- app/src/main/java/org/broadinstitute/clinicapp/util/NetworkUtils.kt
+++ app/src/main/java/org/broadinstitute/clinicapp/util/NetworkUtils.kt
@@ -9,9 +9,10 @@ import android.net.ConnectivityManager
 object NetworkUtils {
 
     fun isNetworkConnected(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = cm.activeNetworkInfo
-
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+//        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        val activeNetwork = cm.activeNetworkInfo
+//
+//        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+        return false;
     }
 }
