6469b8c6723ab9562c7637aed5db5f7dbd673711
codeanticode <andres.colubri@gmail.com>
2023-02-11 07:59:54 -0500

    moved value of web_client_id to properties file

--- app/src/main/java/org/broadinstitute/clinicapp/ui/login/LoginActivity.kt
+++ app/src/main/java/org/broadinstitute/clinicapp/ui/login/LoginActivity.kt
@@ -38,7 +38,7 @@ class LoginActivity : BaseActivity() {
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                     .setSupported(true)
                     // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.web_client_id))
+                    .setServerClientId(org.broadinstitute.clinicapp.BuildConfig.web_client_id)
                     // Show all accounts on the device.
                     .setFilterByAuthorizedAccounts(false)
                     .build()
