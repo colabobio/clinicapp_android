882e64829f5064ff6f12a21fbd55a3e6973fca80
codeanticode <andres.colubri@gmail.com>
2023-02-11 18:03:21 -0500

    using google sig-in

--- app/src/main/java/org/broadinstitute/clinicapp/ui/login/LoginActivity.kt
+++ app/src/main/java/org/broadinstitute/clinicapp/ui/login/LoginActivity.kt
@@ -1,29 +1,25 @@
 package org.broadinstitute.clinicapp.ui.login
 
 import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import org.broadinstitute.clinicapp.ClinicApp
+import org.broadinstitute.clinicapp.Constants
 import org.broadinstitute.clinicapp.R
 import org.broadinstitute.clinicapp.base.BaseActivity
-
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.CommonStatusCodes
-
-import org.broadinstitute.clinicapp.Constants
 import org.broadinstitute.clinicapp.ui.home.HomeActivity
-import org.broadinstitute.clinicapp.ClinicApp
 
 class LoginActivity : BaseActivity() {
-    lateinit var oneTapClient: SignInClient
-    lateinit var signInRequest: BeginSignInRequest
-
-    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
-    private var showOneTapUI = true
+    private lateinit var signInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 612  // Can be any integer unique to the Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,100 +27,64 @@ class LoginActivity : BaseActivity() {
         setUp()
     }
 
-    override fun setUp() {
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(org.broadinstitute.clinicapp.BuildConfig.web_client_id)
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
+    override fun onStart() {
+        super.onStart()
+
+        var button = findViewById<SignInButton>(R.id.sign_in_button)
+        button.setOnClickListener {
+            signIn()
+        }
     }
 
+    override fun setUp() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
 
-    fun signIn(view: View) {
+        signInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    fun signIn() {
         // Signing out just in case the user is returning from the home screen
-        oneTapClient.signOut()
+        signInClient.signOut()
 
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(this) { result ->
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQ_ONE_TAP,
-                        null, 0, 0, 0, null)
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e("ClinicApp signIn", "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            .addOnFailureListener(this) { e ->
-                // No saved credentials found. Launch the One Tap sign-up flow, or
-                // do nothing and continue presenting the signed-out UI.
-                Log.d("ClinicApp signIn", e.localizedMessage)
-            }
+        val signInIntent: Intent = signInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = credential.googleIdToken
-                    val username = credential.id
-                    val password = credential.password
-                    when {
-                        idToken != null -> {
-                            // Got an ID token from Google. Use it to authenticate
-                            // with your backend.
-                            Log.d("ClinicApp signIn", "Got ID token.")
-                        }
-                        password != null -> {
-                            // Got a saved username and password. Use them to authenticate
-                            // with your backend.
-                            Log.d("ClinicApp signIn", "Got password.")
-                        }
-                        else -> {
-                            // Shouldn't happen.
-                            Log.d("ClinicApp signIn", "No ID token or password!")
-                        }
-                    }
-
-                    pref  =  ClinicApp.instance!!.getPrefStorage()
-                    pref.writeStringToPref(Constants.PrefKey.PREF_USER_NAME, username)
-                    pref.writeBooleanToPref(Constants.PrefKey.PREF_USER_CREATED, true)
-
-                    handleUserLogin()
-                } catch (e: ApiException) {
-                    when (e.statusCode) {
-                        CommonStatusCodes.CANCELED -> {
-                            Log.d("ClinicApp signIn", "One-tap dialog was closed.")
-                            // Don't re-prompt the user.
-                            showOneTapUI = false
-                        }
-                        CommonStatusCodes.NETWORK_ERROR -> {
-                            Log.d("ClinicApp signIn", "One-tap encountered a network error.")
-                            // Try again or just ignore.
-                        }
-                        else -> {
-                            Log.d("ClinicApp signIn", "Couldn't get credential from result." +
-                                    " (${e.localizedMessage})")
-                        }
-                    }
-                }
-            }
+        if (requestCode === RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
         }
     }
 
+    fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
 
-    fun handleUserLogin() {
-        intent = Intent(this, HomeActivity::class.java)
-            .putExtra(Constants.BundleKey.HOME_ACTIVITY_KEY, Constants.BundleKey.HOME_CALL_FROM_LOGIN)
-        startActivity(intent)
+            // Signed in successfully, show authenticated UI.
+
+            pref = ClinicApp.instance!!.getPrefStorage()
+            account.id?.let {
+                pref.writeStringToPref(Constants.PrefKey.PREF_USER_NAME, it)
+                pref.writeBooleanToPref(Constants.PrefKey.PREF_USER_CREATED, true)
+            }
+
+            intent = Intent(this, HomeActivity::class.java)
+                .putExtra(Constants.BundleKey.HOME_ACTIVITY_KEY, Constants.BundleKey.HOME_CALL_FROM_LOGIN)
+            startActivity(intent)
+
+
+//        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Clinicapp", "signInResult:failed code=" + e.getStatusCode())
+        }
     }
 }
